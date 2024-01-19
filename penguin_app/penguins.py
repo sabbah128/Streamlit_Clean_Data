2aec867073eae99283d43144f51a227310cbec32
sabbah128 <sabbah128@gmail.com>
2024-01-19 08:56:51 -0800

    new app penguin

---
+++ penguin_app/penguins.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import pandas as pd
+import altair as alt
+import seaborn as sns
+
+
+st.title("Palmer's Penguins")
+
+st.markdown('Use this Streamlit app to make your own scatterplot about penguins!')
+
+selected_species = st.selectbox('What species would you like to visualize?', 
+                                ['Adelie', 'Gentoo', 'Chinstrap'])
+selected_x_var = st.selectbox('What do you want the x variable to be?', 
+                              ['bill_length_mm', 'bill_depth_mm', 'flipper_length_mm', 'body_mass_g'])
+selected_y_var = st.selectbox('What about the y?', 
+                              ['bill_length_mm', 'bill_depth_mm', 'flipper_length_mm', 'body_mass_g'])
