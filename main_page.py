63938e32a6c79e4ec2f5954c6bbbaa6380981d9d
sabbah128 <sabbah128@gmail.com>
2024-01-19 01:34:38 -0800

    gitignore update

--- main_page.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-import streamlit as st
-import matplotlib
-
-
-url = '.\Final_TPA_data.xlsx'
-df = pd.read_excel(st.file_uploader('Insert your file:', type=["csv", "xlsx", "txt"]))
-st.write(df.head(7))
