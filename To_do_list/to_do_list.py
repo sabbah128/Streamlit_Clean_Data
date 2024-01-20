9c5ccbf6eaf7b293e987d6f4c7b6c3f738a909ee
sabbah128 <sabbah128@gmail.com>
2024-01-20 07:06:10 -0800

    Update to_do_list.py

--- To_do_list/to_do_list.py
+++ To_do_list/to_do_list.py
@@ -4,11 +4,11 @@ import streamlit as st
 st.title('My To-Do List Creator')
 
 if 'my_todo_list' not in st.session_state:
-    st.session_state.my_todo_list = ["Buy groceries", "Learn Streamlit", "Learn Python"]
+    st.session_state.my_todo_list = []
 
 new_todo = st.text_input("What do you need to do?")
 
-if st.button('Add the new To-Do item'):
+if st.button('Add the new To-Do item', type='primary'):
     st.write('Adding a new item to the list')
     st.session_state.my_todo_list.append(new_todo)
 
