eff9bed35bdbd9d123d6bc17be05dcf9e6a8636f
sabbah128 <sabbah128@gmail.com>
2024-01-20 06:52:20 -0800

    new file To do list

--- penguin_app/penguins.py
+++ penguin_app/penguins.py
@@ -4,28 +4,40 @@ import altair as alt
 import seaborn as sns
 
 
+@st.cache_data()
+def load_df(penguin_file):
+    if penguin_file is not None:
+        penguins_df = pd.read_csv(penguin_file)
+    else:
+        st.write('Data has not inserted yet!')
+        st.stop()
+    return penguins_df
+
 st.title("Palmer's Penguins")
 st.markdown('Use this Streamlit app to make your own scatterplot about penguins!')
 
-penguin_file = st.file_uploader('Select Your Local Penguins CSV')
+penguins_df = load_df(st.file_uploader('Select Your Local Penguins CSV'))
 
-if penguin_file is not None:
-    penguins_df = pd.read_csv(penguin_file)
-    selected_x_var = st.selectbox('What do you want the x variable to be?', 
-                                ['bill_length_mm', 'bill_depth_mm', 'flipper_length_mm', 'body_mass_g'])
-    selected_y_var = st.selectbox('What about the y?', 
-                                ['bill_depth_mm', 'bill_length_mm', 'flipper_length_mm', 'body_mass_g'])
+selected_x_var = st.selectbox('What do you want the x variable to be?', 
+                            ['bill_length_mm', 'bill_depth_mm', 'flipper_length_mm', 'body_mass_g'])
+selected_y_var = st.selectbox('What about the y?', 
+                            ['bill_depth_mm', 'bill_length_mm', 'flipper_length_mm', 'body_mass_g'])
+selected_gender = st.selectbox('What gender do you want to filter for?',
+                                ['All genguins', 'Male penguins', 'Female penguins'])
 
+if selected_gender == 'Male penguins':
+    penguins_df = penguins_df[penguins_df['sex'] == 'male']
+    shape_sex = 'triangle'
+elif selected_gender == 'Female penguins':
+    penguins_df = penguins_df[penguins_df['sex'] == 'female']
+    shape_sex = 'square'
 else:
-    st.write('Data has not inserted yet!')
-    st.stop()
+    shape_sex = 'circle'
 
-sns.set_style('darkgrid')
-markers = {"Adelie": "X", "Gentoo": "s", "Chinstrap":'o'}
-alt_chart = (alt.Chart(penguins_df, 
+alt_chart = (alt.Chart(penguins_df,                       
                        title="Scatterplot of Palmer's Penguins")
-                       .mark_circle()
-                       .encode(x=selected_x_var,y=selected_y_var,color="species",)
+                       .mark_point(shape=shape_sex)
+                       .encode(x=selected_x_var, y=selected_y_var, color="species",)
                        .interactive())
 
 st.altair_chart(alt_chart, use_container_width=True)
